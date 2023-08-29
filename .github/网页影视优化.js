0ecda16f6fca61bc41695ec679a93a21f2377c15
madasen <43581108+madasen@users.noreply.github.com>
2023-08-29 20:14:02 +0800

    Add files via upload

---
+++ .github/网页影视优化.js
@@ -0,0 +1,43 @@
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/cokemv.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/pornhub.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/lezhu.css" type="text/css">')
+    .replace(/jquerys.js\?v/g, "ddgksf2013.js?v");
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/bimi.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/18comic.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/555.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/gimytv.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/tkys.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/qianoo.css" type="text/css">');
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'");
+$done({ body });
+/* ����桁※蕷惹��莅� crated by ddgksf2013 on 2022-05-28 */
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/libvio.css" type="text/css">');
+$done({ body });
